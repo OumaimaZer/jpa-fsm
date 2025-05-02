18540a9f246aaced258d332d459b63c75c380ca2
OumaimaZer <o.zerhouani@edu.umi.ac.ma>
2025-05-02 22:03:04 +0100

    First Try

---
+++ src/main/java/net/zerhouani/jpafsm/JpaFsmApplication.java
@@ -0,0 +1,13 @@
+package net.zerhouani.jpafsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaFsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaFsmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/zerhouani/jpafsm/JpaFsmApplicationTests.java
@@ -0,0 +1,13 @@
+package net.zerhouani.jpafsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaFsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
